8391e4f78869a5ef229945c678b35c890fce1b85
League Student <league@unknown28f076787fa4.attlocal.net>
2019-09-14 12:54:04 -0700

    :smile:

--- src/_04_chuckle_clicker/ChuckleClicker.java
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -1,12 +1,17 @@
 package _04_chuckle_clicker;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
+public class ChuckleClicker implements ActionListener {
 
+	JButton button = new JButton();
+	JButton button2 = new JButton();
 	public static void main(String[] args) {
 		ChuckleClicker cc = new ChuckleClicker();
 		cc.makeButtons();
@@ -16,15 +21,28 @@ public class ChuckleClicker {
 		JFrame frame = new JFrame();
 		frame.setVisible(true);
 		JPanel panel = new JPanel();
-		JButton button = new JButton();
-		JButton button2 = new JButton();
+	
 		frame.add(panel);
-		frame.add(button);
-		frame.add(button2);
+		panel.add(button);
+		panel.add(button2);
 		button.setText("joke");
 		button2.setText("punchline");
 		button.setSize(100, 100);
 		button2.setSize(100,100);
-		
+		button.addActionListener(this);
+		button2.addActionListener(this);
+		frame.pack();
+				
+	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+			JOptionPane.showMessageDialog(null, "hi");
+if (e.getSource() == button) {
+	JOptionPane.showMessageDialog(null, "Why did the scarecrow win an award? Because he was outstanding in his field!");
+}
+else if (e.getSource() == button2) {
+	JOptionPane.showMessageDialog(null, "Do it right for less");
+}
 	}
 }
---
+++ src/_05_typing_tutor/TypingTutor.java
@@ -0,0 +1,75 @@
+package _05_typing_tutor;
+
+
+import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TypingTutor implements KeyListener {
+	static JFrame frame = new JFrame();
+	static JLabel label = new JLabel();
+	static char currentLetter;
+	JPanel panel = new JPanel();
+	
+public static void main(String[] args) {
+	currentLetter = generateRandomLetter();
+label.setText(currentLetter+"");
+label.setFont(label.getFont().deriveFont(28.0f));
+label.setHorizontalAlignment(JLabel.CENTER);
+frame.setVisible(true);
+frame.add(label);
+frame.pack();
+TypingTutor typingtutor = new TypingTutor();
+typingtutor.KeyListner();
+}
+	
+
+	
+	static char generateRandomLetter() {
+	      Random r = new Random();
+	      return (char) (r.nextInt(26) + 'a');
+	     
+	}
+	void KeyListner(){
+		frame.addKeyListener(this);
+	}
+
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyChar()==((currentLetter))) {
+			System.out.println("correct");
+			panel.setBackground(Color.green);
+		}
+		else {
+			System.out.println("incorrect");
+			panel.setBackground(Color.red);
+		}
+	}
+
+
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println(label);
+		
+	}
+
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		currentLetter = generateRandomLetter();
+		label.setText(currentLetter+"");
+	}
+
+}
