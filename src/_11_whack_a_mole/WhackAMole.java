d06a16a3c20d84f9e8058527fde16875d58b9870
League Student <league@iMac-16.attlocal.net>
2019-11-02 13:00:23 -0700

    :sleeping:

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,5 +1,7 @@
 package _11_whack_a_mole;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Random;
 
 import javax.swing.JButton;
@@ -7,16 +9,46 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
 JLabel label = new JLabel();
 JButton button = new JButton();
 
-void drawButtons(Random number) {
+
+void drawButtons() {
+	Random random = new Random();
+	
+	int elmo = random.nextInt(24);
+	for (int i = 0; i < 24; i++) {
+		JButton button = new JButton();
+		panel.add(button);
+		if (elmo==i) {
+			button.setText("mole!");
+		}
+	
+	}
 	frame.setVisible(true);
-	frame.setSize(100, 100);
+	frame.setSize(500, 500);
 	frame.add(panel);
-	panel.add(button);
+	panel.add(label);
+	button.addActionListener(this);
 }
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	if (e.getSource()==button) {
+		speak("You missed");
+	}
+	
+	
+}
+void speak(String words) { 
+    try { 
+        Runtime.getRuntime().exec("say " + words).waitFor();
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
+}	
 }
---
+++ src/_11_whack_a_mole/WhackAMoleRunner.java
@@ -0,0 +1,13 @@
+package _11_whack_a_mole;
+
+import java.util.Random;
+
+public class WhackAMoleRunner {
+
+	public static void main(String[] args) {
+		
+		
+		WhackAMole whackamole = new WhackAMole();
+		whackamole.drawButtons();
+	}
+}
