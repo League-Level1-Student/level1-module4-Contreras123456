05484356f88ddf84040fc40c60377918d3776494
League Student <league@iMac-16.attlocal.net>
2019-11-23 12:55:07 -0800

    :+1:

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -1,7 +1,10 @@
 package _12_slot_machine;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -10,37 +13,55 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class SlotMachine {
+public class SlotMachine implements ActionListener {
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
 JButton button = new JButton();
 JLabel label = new JLabel();
 JLabel label2 = new JLabel();
 JLabel label3 = new JLabel();
-String picture = "cherry.jpg";
-String picture2 = "orange.jpg";
-String picture3 = "lime.jpg";
+static String picture = "cherry.jpg";
+static String picture2 = "seven.jpg";
+static String picture3 = "bar.jpg";
 
 
-	public static void main(String[] args) {
 	
-	}
 
-	 void setup() {
+void setup() {
 		// TODO Auto-generated method stub
-frame.add(panel);
-panel.add(label);
-panel.add(button);
-button.setText("Spin");
 try {
 	label = createLabelImage(picture);
-	label = createLabelImage(picture2);
-	label = createLabelImage(picture3);
+	label2 = createLabelImage(picture2);
+	label3 = createLabelImage(picture3);
 } catch (MalformedURLException e) {
 	// TODO Auto-generated catch block
 	e.printStackTrace();
 }
+frame.setVisible(true);
+frame.add(panel);
+panel.add(label);
+panel.add(label2);
+panel.add(label3);
+panel.add(button);
+button.setText("Spin");
+button.addActionListener(this);
+frame.pack();
+
 	}
+
+static String randomPic() {
+	Random random = new Random();
+	int kevin = random.nextInt(3);
+	if (kevin==0) {
+		return picture;
+	}
+	else if (kevin==1) {
+		return picture2;
+	}
+	else {
+		return picture3;
+	}
+}
 	 private JLabel createLabelImage(String fileName) throws MalformedURLException{
 	        URL imageURL = getClass().getResource(fileName);
 		if (imageURL == null){
@@ -50,5 +71,29 @@ try {
 		Icon icon = new ImageIcon(imageURL);
 		JLabel imageLabel = new JLabel(icon);
 		return imageLabel;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		String action = randomPic();
+		String lego = randomPic();
+		String wiiremote = randomPic();
+		panel.remove(label);
+		panel.remove(label2);
+		panel.remove(label3);
+		try {
+			label = createLabelImage(action);
+			label2 = createLabelImage(lego);
+			label3 = createLabelImage(wiiremote);
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		panel.add(label);
+		panel.add(label2);
+		panel.add(label3);
+		frame.pack();
 	}
 }
