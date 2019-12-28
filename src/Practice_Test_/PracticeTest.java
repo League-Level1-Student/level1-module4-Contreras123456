d7079d89d1dd97dd243bbb7d6a4b8fefaf34c9bd
League Student <league@iMac-16.attlocal.net>
2019-12-28 12:57:21 -0800

    :smile:

--- src/Practice_Test_/PracticeTest.java
+++ src/Practice_Test_/PracticeTest.java
@@ -1,10 +1,12 @@
 package Practice_Test_;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -13,23 +15,43 @@ public class PracticeTest implements ActionListener {
   JLabel label = new JLabel();
   JButton button = new JButton();
   JButton button2 = new JButton();
-  JTextField textfield = new JTextField();
+  JTextField textfield = new JTextField(10);
   JPanel panel = new JPanel();
-
+public static void main(String[] args) {
+	PracticeTest practicetest = new PracticeTest();
+	practicetest.setup();
+}
 	void setup() {
 		frame.setVisible(true);
-		frame.add(panel);
 		panel.add(label);
 		panel.add(button);
 		panel.add(button2);
 		panel.add(textfield);
+		label.setText("What month of the year has 28 days?");
+		button.setText("submit");
+		button2.setText("hint");
+		label.setPreferredSize(new Dimension(250,50));
+		frame.add(panel);
 		button.addActionListener(this);
+		button2.addActionListener(this);
 		frame.pack();
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		if (e.getSource()==button) {
+			if (textfield.getText().equals("all of it")) {
+				JOptionPane.showMessageDialog(null, "Correct");
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}
+		}
+		else if (e.getSource()==button2) {
+			JOptionPane.showMessageDialog(null, "How many months have 28 days");
+		}
+			}
 		
 	}
-}
+
